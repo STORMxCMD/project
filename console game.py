915c803afb2b508440fad8bbbaf1d36cb06faa3b
STORMxCMD <usmonovscamn1@gmail.com>
2025-02-24 15:26:50 +0500

    Update console game.py

--- console game.py
+++ console game.py
@@ -3,16 +3,18 @@
 import random
 
 secret_number = random.randint(1, 100)
+urinishlar=0
 
 print("Men 1 dan 100 gacha son o'yladim uni toping")
 
 while True:
     guess = int(input("Taxminingiz: "))
+    urinishlar+=1
     
     if guess < secret_number:
         print("Juda kichik! Yana urinib ko‘ring")
     elif guess > secret_number:
         print("Juda katta! Yana urinib ko‘ring")
     else:
-        print("Tabriklayman! To‘g‘ri topdingiz!")
+        print(f"Tabriklayman! Siz {urinishlar}ta urinishda topdingiz!")
         break
